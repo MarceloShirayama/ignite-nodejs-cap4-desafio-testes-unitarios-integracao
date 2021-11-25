1410d620b525d0027240261c7c36aeda07686d01
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-25 12:30:55 -0300

    Ensure Authenticate User Use Case not be able to authenticate an nonexistent user

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../shared/errors/AppError"
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
 import { IUsersRepository } from "../../repositories/IUsersRepository"
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
@@ -32,5 +33,21 @@ describe('Authenticate User Use Case', () => {
     expect(token).toHaveProperty('token')
     expect(token).toHaveProperty('user.name', 'any name')
     expect(token).toHaveProperty('user.email', 'any_email@mail.com')
+  })
+
+
+
+  it('Should not be able to authenticate an nonexistent user', async () => {
+    expect(async () => {
+      const userFake: ICreateUserDTO = {
+        name: 'any name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+
+      const token = await authenticateUserUseCase.execute(
+        { email: userFake.email, password: userFake.password}
+      )
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
