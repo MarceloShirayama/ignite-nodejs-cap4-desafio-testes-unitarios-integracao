e700894136639e1647f4787e7d139b361c6a6d2e
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-26 15:15:19 -0300

    Ensure CreateUserController not be able to create a new user with an already registered email

--- src/modules/users/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -7,6 +7,12 @@ import { ICreateUserDTO } from "./ICreateUserDTO";
 let connection: Connection;
 
 describe("CreateUserController", () => {
+  const userFake: ICreateUserDTO = {
+    name: "any name",
+    email: "any_email@mail.com",
+    password: "any_password",
+  };
+
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -18,12 +24,13 @@ describe("CreateUserController", () => {
   });
 
   it("Should be able to create a new user", async () => {
-    const userFake: ICreateUserDTO = {
-      name: "any name",
-      email: "any_email@mail.com",
-      password: "any_password",
-    };
-
     await request(app).post("/api/v1/users").send(userFake).expect(201);
+  });
+
+  it("Should not be able to create a new user with an already registered email", async () => {
+    const response = await request(app).post("/api/v1/users").send(userFake);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: "User already exists" });
   });
 });
