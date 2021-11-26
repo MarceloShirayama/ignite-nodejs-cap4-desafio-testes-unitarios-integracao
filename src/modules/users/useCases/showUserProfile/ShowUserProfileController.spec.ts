776cb51dc1aa3fbfa8488f1e5745dbc344dba370
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-26 16:16:30 -0300

    Ensure ShowUserProfileController  be able show user profile

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
@@ -0,0 +1,49 @@
+import { Response } from "superagent";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { app } from "../../../../app";
+import createConnection from "../../../../database";
+import { ICreateUserDTO } from "../createUser/ICreateUserDTO";
+
+let connection: Connection;
+
+const userFake: ICreateUserDTO = {
+  name: "any name",
+  email: "any_email@mail.com",
+  password: "any_password",
+};
+
+let responseAuthenticate: Response;
+
+const { email, password } = userFake;
+
+describe("ShowUserProfileController", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+    await request(app).post("/api/v1/users").send(userFake);
+    responseAuthenticate = await request(app)
+      .post("/api/v1/sessions")
+      .send({ email, password });
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    connection.close;
+  });
+
+  it("Should be able show user profile", async () => {
+    const { token } = responseAuthenticate.body;
+    const response = await request(app)
+      .get("/api/v1/profile")
+      .send()
+      .set({ authorization: `Bearer ${token}` });
+
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty("id")
+    expect(response.body).toHaveProperty("name", "any name")
+    expect(response.body).toHaveProperty("email", "any_email@mail.com")
+    expect(response.body).toHaveProperty("created_at")
+    expect(response.body).toHaveProperty("updated_at")
+  });
+});
